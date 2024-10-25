f6d23e97fb1e922dddb826bf3bb3658eb51f9df4
hard-nett <hardnettt@proton.me>
2024-10-25 02:51:22 +0000

    cargo fmt

--- contracts/bs721-account-marketplace/src/bin/schema.rs
+++ contracts/bs721-account-marketplace/src/bin/schema.rs
@@ -1,8 +1,6 @@
 use bs721_account_marketplace::msgs::{ExecuteMsg, InstantiateMsg, QueryMsg};
 use cosmwasm_schema::write_api;
 
-
-
 fn main() {
     write_api! {
         instantiate: InstantiateMsg,
