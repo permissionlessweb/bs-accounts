27cc7342d43dcea4834bde49d5bc145c0233d771
hard-nett <hardnettt@proton.me>
2025-06-02 09:52:10 -0400

    wip: add smart-account demos

--- contracts/smart-accounts/btsg-ed25519/src/msg.rs
+++ contracts/smart-accounts/btsg-ed25519/src/msg.rs
@@ -1,12 +1,11 @@
 use btsg_auth::AuthenticatorSudoMsg;
 use cosmwasm_schema::{cw_serde, QueryResponses};
-use cosmwasm_std::{Addr, Binary};
+use cosmwasm_std::Addr;
 
 #[cw_serde]
 pub struct InstantiateMsg {
     pub owner: Option<Addr>,
-    pub wavs_operator_pubkeys: Vec<Binary>,
-    pub wavs_pubkey_type: String,
+    pub pubkey: String,
 }
 
 #[cw_serde]
