13653e07905e26e5fb6a3e1764b5b5fddd93777f
hard-nett <hardnettt@proton.me>
2025-09-28 00:41:05 -0400

    add artifacts

--- scripts/sh/optimize.sh
+++ scripts/sh/optimize.sh
@@ -1,4 +1,5 @@
+#!/bin/sh
   docker run --rm -t -v "$(pwd)":/code \
     --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
     --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-    cosmwasm/optimizer-arm64:0.16.0
+    cosmwasm/optimizer-arm64:0.17.0
